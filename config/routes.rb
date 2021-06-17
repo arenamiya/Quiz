5de9b0c9922362c4d6a72c7931fe2ab4e31a4018
Aren <71926235+arenamiya@users.noreply.github.com>
2021-06-18 02:04:42 +1000

    removed unneccessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'quiz#index'
-  get '/quiz', to: 'quiz#quiz'
   post '/submit', to: 'quiz#submit'
   get '/complete', to: 'quiz#complete'
 end
--- db/migrate/20210617133102_drop_question.rb
+++
@@ -1,4 +0,0 @@
-class DropQuestion < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
